d205c9bcf9414082adf247d95b83ffaab29a4c17
luis0283 <luis0283@outlook.com>
2021-08-02 15:58:52 -0600

    configurando el html primera parte

--- figuras.js
+++ figuras.js
@@ -84,3 +84,22 @@ function areaCirculo(radio){
     return (radio * radio) * PI;
 }
 console.groupEnd();
+
+//configurando el html
+
+function calcularPerimetroCuadrado()
+{
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+function calcularAreaCuadrado()
+{
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    
+    const area = areaCuadrado(value);
+    alert(area);
+}
